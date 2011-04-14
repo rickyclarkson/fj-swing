01a4a79e7a652a2fe7554fb4554f35ceb6a75244
Ricky Clarkson <richard.clarkson@globant.com>
2011-04-14 14:12:00 -0300

    Removing duplicate code.

--- src/test/java/fj/swing/TextFieldTest.java
+++ src/test/java/fj/swing/TextFieldTest.java
@@ -1,31 +1,35 @@
 package fj.swing;
 
 import javax.swing.JTextField;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public final class TextFieldTest {
+    private Value<String> value;
+    private JTextField field;
+
+    @Before
+    public void setUp() {
+        value = new Value<String>("foo");
+        field = TextFieldW.textField(new JTextField()).bind(value).unwrap();
+    }
+
     @Test
     public void changeText() {
-        final Value<String> value = new Value<String>("foo");
-        final JTextField field = TextFieldW.textField(new JTextField()).bind(value).unwrap();
         field.setText("bar");
         assertEquals("The value should be bar, reflecting the change to the textfield", "bar", value.get());
     }
 
     @Test
     public void changeValue() {
-        final Value<String> value = new Value<String>("foo");
-        final JTextField field = TextFieldW.textField(new JTextField()).bind(value).unwrap();
         value.set("baz");
         assertEquals("The textfield should be showing baz, reflecting the change to the value", "baz", field.getText());
     }
 
     @Test
     public void initialValue() {
-        final Value<String> value = new Value<String>("foo");
-        final JTextField field = TextFieldW.textField(new JTextField()).bind(value).unwrap();
         assertEquals("The textfield should be showing foo, as that is the value bound to it", "foo", field.getText());
     }
 }
